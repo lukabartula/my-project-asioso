093c5e7ec033825d87a4cf0a9eecd964bb36ca3a
Luka <luka.bartula@stu.ibu.edu.ba>
2025-04-18 17:53:48 +0200

    initial commit

---
+++ src/Controller/ContentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Bridge\Twig\Attribute\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContentController extends FrontendController
+{
+    #[Template('content/index.html.twig')]
+    public function defaultAction(Request $request): array
+    {
+        return [];
+    }
+}
+
